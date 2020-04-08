43c299e38cba40c1d687c9e012210120bdacff54
hongweipeng <hongweichen8888@sina.com>
2020-04-08 12:48:35 +0800

    整站变灰支持设置日期

--- usr/plugins/SiteGray/Plugin.php
+++ usr/plugins/SiteGray/Plugin.php
@@ -43,6 +43,8 @@ class SiteGray_Plugin implements Typecho_Plugin_Interface
     public static function config(Typecho_Widget_Helper_Form $form)
     {
         /** 分类名称 */
+        $gray_dates = new Typecho_Widget_Helper_Form_Element_Textarea('gray_dates', NULL, '', _t('变灰日期'), _t('填入形如"04-04" 或 "2020-04-04"日期，一行一个，不指定年份表示每年'));
+        $form->addInput($gray_dates);
     }
 
     /**
@@ -62,8 +64,25 @@ class SiteGray_Plugin implements Typecho_Plugin_Interface
      */
     public static function header()
     {
-        $cssUrl = Helper::options()->pluginUrl . '/SiteGray/site_gray.css';
-        echo '<link rel="stylesheet" type="text/css" href="' . $cssUrl . '" />';
+        $gray_dates=Typecho_Widget::widget('Widget_Options')->plugin('SiteGray')->gray_dates;
+        $date_arr = explode("\n", $gray_dates );
+        if (static::match($date_arr)) {
+            $cssUrl = Helper::options()->pluginUrl . '/SiteGray/site_gray.css';
+            echo '<meta name="theme-color" content="999999">';
+            echo '<link rel="stylesheet" type="text/css" href="' . $cssUrl . '" />';
+        }
+    }
+
+    public static function match($date_arr) {
+        $today1 = date('Y-m-d');
+        $today2 = date('m-d');
+        foreach ($date_arr as $date) {
+            $date = trim($date);
+            if ($date == $today1 || $date == $today2) {
+                return true;
+            }
+        }
+        return false;
     }
 
 }
